da6b927c1936413a3906e00af9bb51731fd3719f
timol085 <timol085@student.liu.se>
2023-02-10 13:33:28 +0100

    FIxed merge conflict

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from matplotlib import pyplot as plt
 from funcitons import get_augmented_images_cropped, crop_image
 
 # Load the video
-<<<<<<< Updated upstream
+<< << << < Updated upstream
 cap = cv2.VideoCapture("video.mp4")
-=======
+== == == =
 cap = cv2.VideoCapture("TestCameravsPhone.mp4")
->>>>>>> Stashed changes
+>>>>>> > Stashed changes
 
 # Get the first frame
 ret, frame = cap.read()
@@ -15,19 +15,19 @@ ret, frame = cap.read()
 # Display the frame and wait for user to select a region
 crop_data = cv2.selectROI(frame)
 
-x,y,w,h = crop_data
+x, y, w, h = crop_data
 
 
 # Crop the selected region and save it as an image
-crop = crop_image(frame,x,y,w,h)
+crop = crop_image(frame, x, y, w, h)
 cv2.imwrite("selected_region.jpg", crop)
 
-augmented_images_cropped = get_augmented_images_cropped(12, frame,crop_data)
+augmented_images_cropped = get_augmented_images_cropped(12, frame, crop_data)
 
 for cropped_img in augmented_images_cropped:
     plt.imshow(cropped_img, cmap=plt.get_cmap('gray'))
     plt.show()
-    
+
 # Clean up
 cap.release()
 cv2.destroyAllWindows()
