87ebbaece74e8bd5bc7c6525cd3af2d7377e0913
Mathilda <matwe866@student.liu.se>
2023-02-24 17:54:11 +0100

    Made changes in track and detection

--- main_mosse.py
+++ main_mosse.py
@@ -1,8 +1,9 @@
 from mossetracker import MosseTracker
 import cv2
 
-tracker = MosseTracker(cv2_color=cv2.COLOR_BGR2HSV)
+tracker = MosseTracker(cv2_color=cv2.COLOR_BGR2HSV,resnet=True)
 
-tracker.initialize("./video_sequences/surfer.mp4")
-
+tracker.initialize("./video_sequences/rihanna.mp4", useDetection=True)
+print("hello")
 tracker.track()
+print("hello")
--- mossetracker.py
+++ mossetracker.py
@@ -11,7 +11,7 @@ from matplotlib import animation
 
 
 class MosseTracker:
-    def __init__(self, cv2_color=cv2.COLOR_GRAY2BGR):
+    def __init__(self, cv2_color=cv2.COLOR_GRAY2BGR, hog=False, resnet=False):
         self.color_mode = cv2_color
         self.video_url = None
         self.first_frame = None
@@ -19,31 +19,48 @@ class MosseTracker:
         self.video = None
         self.selected_region = None
         self.useDetection = None
+        self.HOG = hog
+        self.ResNes = resnet
 
     def initialize(self, video_url, useDetection=False):
         self.video_url = video_url
         self.read_first_frame()
         self.useDetection = useDetection
-
+        
         # do eiter detection or let user select regions
         if self.useDetection == False:
-            x, y, w, h = self.get_selected_region(self.first_frame, False)
-
+            self.initialize_with_region()
+        else:
+            self.initialize_with_detection()
+            
+                
+    def initialize_with_region(self):
+        x, y, w, h = self.get_selected_region(self.first_frame, False)
+        self.selected_region = (x, y, w, h)
+        augmented_images = self.augmented_images(12, self.first_frame, (x, y, w, h))
+        self.filter = self.create_filter(augmented_images)
+        
+    def initialize_with_detection(self):
+        returnvalue = self.get_selected_region(self.first_frame, True)
+        if returnvalue != 1:
+            x, y, w, h = returnvalue
             self.selected_region = (x, y, w, h)
-            augmented_images = self.augmented_images(
-                12, self.first_frame, (x, y, w, h))
+            augmented_images = self.augmented_images(12, self.first_frame, (x, y, w, h))
             self.filter = self.create_filter(augmented_images)
         else:
-            returnvalue = self.get_selected_region(self.first_frame, True)
-            if returnvalue != 1:
-                x, y, w, h = returnvalue
-                self.selected_region = (x, y, w, h)
-                augmented_images = self.augmented_images(
-                    12, self.first_frame, (x, y, w, h))
-                self.filter = self.create_filter(augmented_images)
-            else:
-                # if it cannot find anything to detect, it will ask the user
-                self.initialize(video_url)
+            # if it cannot find anything to detect, it will take next frame and try again
+            cap=self.read_first_frame()   
+            canRead,self.first_frame=cap.read()
+            while canRead:
+                ret = self.get_selected_region(self.first_frame, True)
+                if ret !=1: 
+                    canRead=False 
+                    x, y, w, h = returnvalue
+                    self.selected_region = (x, y, w, h)
+                canRead,self.first_frame=cap.read()
+
+            print("Cant find")
+        
 
     def read_first_frame(self):
         cap = cv2.VideoCapture(self.video_url)
@@ -66,46 +83,34 @@ class MosseTracker:
         print(image_height)
         success = True
         peak = []
-        ox, oy, ow, oh = self.selected_region
+        
+        if self.selected_region!= None:
+            ox, oy, ow, oh = self.selected_region
+        else:
+            success=False
+        
         frames = []
         fig, ax = plt.subplots()
         count = 1
         while success:
             success, next_frame = cap.read()
+            if not success:
+                break
             print(self.selected_region)
             x, y, w, h = self.selected_region
             img_color_mode = cv2.cvtColor(crop_image(next_frame, x, y, w, h), self.color_mode).astype(np.float64)
             _, _, num_channels = img_color_mode.shape
-            if not success:
-                break
-            output = None
+            
+
             all_F = []
             all_G = []
-            for i in range(num_channels):
-                i_img_norm = preprocessing(img_color_mode[:, :, i], width=w, height=h)
-                i_F = np.fft.fft2(i_img_norm)
-                i_output = self.apply_filter(i_F, i)
-                if output is None:
-                    output = i_output
-                else:
-                    output += i_output
-                    print(output.shape)
-
-                all_F.append(i_F)
-                all_G.append(i_output)
+            output=self.preprocess_and_calculate_filters(num_channels, img_color_mode, w, h, all_F, all_G)
                 
             ux, uy = updateWindow(x, y, w, h, output, n_times_occluded)
             self.selected_region = (ux, uy, w, h)
             # Display the image
             im = ax.imshow(next_frame, cmap="brg", animated=True)
-            # Create a Rectangle patch
-            rect = patches.Rectangle(
-                (ux, uy), w, h, linewidth=1, edgecolor='r', facecolor='none')
-            rectOrg = patches.Rectangle(
-                (ox, oy), ow, oh, linewidth=1, edgecolor='g', facecolor='none')
-            # Add the patch to the Axes
-            patch = ax.add_artist(rect)
-            frames.append([im, patch])
+            self.draw_rectangle(ux,uy,w,h, ox,oy, ow, oh, ax, frames, im)
             self.update_filter(all_F,all_G)
             count += 1
             success, next_frame = cap.read()
@@ -128,3 +133,29 @@ class MosseTracker:
         filter_temp = []
         for idx,(f,g) in enumerate(zip(F,G)):
             self.filter[idx] = updateFilter(self.filter[idx][1],self.filter[idx][2], f, g)
+            
+    def preprocess_and_calculate_filters(self,num_channels, img_color_mode, w, h, all_F, all_G):
+        output = None
+        for i in range(num_channels):
+            i_img_norm = preprocessing(img_color_mode[:, :, i], width=w, height=h)
+            i_F = np.fft.fft2(i_img_norm)
+            i_output = self.apply_filter(i_F, i)
+        if output is None:
+            output = i_output
+        else:
+            output += i_output
+            print(output.shape)
+
+        all_F.append(i_F)
+        all_G.append(i_output)
+        return output
+            
+    def draw_rectangle(self,ux,uy,w,h, ox,oy, ow, oh, ax, frames, im):
+        # Create a Rectangle patch
+        rect = patches.Rectangle(
+            (ux, uy), w, h, linewidth=1, edgecolor='r', facecolor='none')
+        rectOrg = patches.Rectangle(
+            (ox, oy), ow, oh, linewidth=1, edgecolor='g', facecolor='none')
+        # Add the patch to the Axes
+        patch = ax.add_artist(rect)
+        frames.append([im, patch])
